7e6a6d8fa3ccc9b0e4178784d4967d44fdda6b0b
Danilo Ribeiro <danilo.ribeiro00d@gmail.com>
2021-02-09 23:39:14 -0300

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,31 @@
-import React, { useState } from 'react';
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import api from './services/api';
 import Header from './components/Header';
 
 function App() {
-    const [projetos, setProjetos] = useState(['Desenvolvimento Web', 'Desenvolvimento Mobile']);
+    const [projetos, setProjetos] = useState([]);
 
-    const handleAddProject = () => {
+    useEffect(() => {
+        api.get('/projects').then(response => setProjetos(response.data));
+    }, []);
+
+    const handleAddProject = async () => {
         // projetos.push(`Novo projeto ${Date.now()}`);
-        setProjetos([...projetos, `Novo projeto ${Date.now()}`])
+        // setProjetos([...projetos, `Novo projeto ${Date.now()}`]);
+        const response = await api.post('/projects', { 
+            title: `Novo projeto ${Date.now()}`, 
+            owner: "Luiz Claudio",
+        });
+        const projeto = response.data;
+        setProjetos([...projetos, projeto]);
     }
 
 	return (
 		<>
 			<Header title='Sistema de Informação' />
             <ul>
-                {projetos.map(projeto => <li key={projeto}>{projeto}</li>)}
+                {projetos.map(projeto => <li key={projeto.id}>{projeto.title}</li>)}
             </ul>
             <button type="button" onClick={handleAddProject}>Adicionar Projeto</button>
 		</>

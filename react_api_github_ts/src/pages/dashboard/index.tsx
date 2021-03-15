6e3d91d6e6b0510ca5acb762d7e7af44ec3a37dc
Danilo Ribeiro <danilo.ribeiro00d@gmail.com>
2021-03-15 19:17:58 -0300

    update

--- react_api_github_ts/src/pages/dashboard/index.tsx
+++ react_api_github_ts/src/pages/dashboard/index.tsx
@@ -1,8 +1,72 @@
-import React from 'react';
-import { Title } from './styles';
+import React, { useState } from 'react';
+import { FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explorer repositórios no GitHub</Title>;
+  const [newRepo, setNewRepo] = useState('');
+  const [repositories, setRepositories] = useState([]);
+
+  function handleAddRepository(): void {}
+  return (
+    <>
+      <img src={logoImg} alt="GitHub Explorer" />
+      <Title>Explorer repositórios no GitHub</Title>
+      <Form action="">
+        <input
+          value={newRepo}
+          onChange={(e): void => setNewRepo(e.target.value)}
+          placeholder="Digite o nome do repositório"
+        />
+        <button type="submit">Pesquisar</button>
+      </Form>
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/68981163?s=460&u=db29de5ecccb9492a8e69ca28b472d2e2b3776d6&v=4"
+            alt="Danilo Ribeiro"
+          />
+          <div>
+            <strong>facebook/react</strong>
+            <p>
+              React is an open source JavaScript library used for designing user
+              interfaces
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/68981163?s=460&u=db29de5ecccb9492a8e69ca28b472d2e2b3776d6&v=4"
+            alt="Danilo Ribeiro"
+          />
+          <div>
+            <strong>facebook/react</strong>
+            <p>
+              React is an open source JavaScript library used for designing user
+              interfaces
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/68981163?s=460&u=db29de5ecccb9492a8e69ca28b472d2e2b3776d6&v=4"
+            alt="Danilo Ribeiro"
+          />
+          <div>
+            <strong>facebook/react</strong>
+            <p>
+              React is an open source JavaScript library used for designing user
+              interfaces
+            </p>
+          </div>
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
